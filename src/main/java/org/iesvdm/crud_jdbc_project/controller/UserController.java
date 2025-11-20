7480e8d3f753131528ef6a1133eae9a9dbb2c554
Juan José Ferreri <jferaco483@g.educaand.es>
2025-11-20 14:22:31 +0100

    A

--- src/main/java/org/iesvdm/crud_jdbc_project/controller/UserController.java
+++ src/main/java/org/iesvdm/crud_jdbc_project/controller/UserController.java
@@ -1,19 +1,59 @@
 package org.iesvdm.crud_jdbc_project.controller;
 
-import org.iesvdm.crud_jdbc_project.DAO.UserDAO;
+
+import org.iesvdm.crud_jdbc_project.model.Piramide;
+import org.iesvdm.crud_jdbc_project.model.User;
 import org.iesvdm.crud_jdbc_project.util.HashUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("user")
 public class UserController {
     private HashUtil hashUtil;
+
     @GetMapping("/user")
-    public String user(Model model, @ModelAttribute UserDAO userDAO){
-        return null;
+    public String user(Model model, @ModelAttribute User user) {
+
+        model.addAttribute("user", user);
+
+        return "/user";
+    }
+
+    @PostMapping("/user")
+    public String userSubmit(Model model, @ModelAttribute User user){
+
+        model.addAttribute("username", user.getUsername());
+
+        model.addAttribute("password", user.getPassword());
+
+        return "/user";
+    }
+
+    @GetMapping("/piramide")
+    public String piramide(Model model, @ModelAttribute Piramide piramide){
+
+        model.addAttribute("numero", piramide);
+
+        return "/piramide";
+
+    }
+
+    @PostMapping("/piramide")
+    public String piramideSubmit(Model model, @ModelAttribute Piramide piramide){
+        int numero = piramide.getNumero();
+        List<Integer> piramideImprimir = IntStream.rangeClosed(1,numero)
+                        .boxed()
+                        .collect(Collectors.toList());
+        model.addAttribute("numero", numero);
+        model.addAttribute("piramideImprimir", piramideImprimir);
+
+        return "/piramide";
+
     }
 }
---
+++ src/main/java/org/iesvdm/crud_jdbc_project/model/Piramide.java
@@ -0,0 +1,15 @@
+package org.iesvdm.crud_jdbc_project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Piramide {
+    private int numero;
+}
